1c575cfdbfee3fce74502c898f73188bf3396e8f
Mario Alemi <mario.alemi@gmail.com>
2014-07-28 11:27:24 +0200

    missing comma

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='cold-start-recommender',
       description='Fast, Ready-to-be-used recommender',
       author='Mario Alemi',
-      author_email='mario.alemi@gmail.com'
+      author_email='mario.alemi@gmail.com',
       version='0.1',
       py_modules=['csrec', 'csrc.Recommender'],
       )
