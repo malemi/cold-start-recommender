3ccb408fdfa428600a44437d83ea16c07a545581
Mario Alemi <mario.alemi@gmail.com>
2014-07-28 19:04:14 +0200

    small changes

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='cold-start-recommender',
-      description='Fast, Ready-to-be-used recommender',
+      description='In-memory recommender for recommendations produced on-the-fly',
       author='Mario Alemi',
       author_email='mario.alemi@gmail.com',
-      version='0.3.2',
+      version='0.3.3',
       py_modules=['csrec.Recommender'],
       url='https://github.com/malemi/cold-start-recommender',
       )
