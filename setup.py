6b347cd4c6ad79f7731fa0ebca9f7547baef036e
Mario Alemi <mario.alemi@gmail.com>
2014-08-01 11:45:08 +0200

    v3.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='cold-start-recommender',
       description='In-memory recommender for recommendations produced on-the-fly',
       author='Mario Alemi',
       author_email='mario.alemi@gmail.com',
-      version='0.3.3',
+      version='0.3.5',
       py_modules=['csrec.Recommender'],
       url='https://github.com/malemi/cold-start-recommender',
       )
